4676f20d783b97e814a392d365ae9bd201e75d9c
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-04-20 13:56:34 -0600

    add endpoint that gets the blog posts data from the markdown files

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,6 +8,15 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	interface Post {
+		title: string;
+		slug: string;
+		description: string;
+		image?: string;
+		date: string;
+		categories: string[];
+		published: boolean;
+	}
 }
 
 export {};
