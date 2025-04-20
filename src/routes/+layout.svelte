c563ba5e59d751da5304a330c923a7829cf67621
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-04-20 10:29:32 -0600

    start creating the landing page for latest blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main class="container mx-auto h-full min-h-screen bg-black px-4 py-8 text-gray-100">
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to My SvelteKit Blog!</h1>
+<a class="text-non-photo-blue" href="/blog">View my blogs!</a>
