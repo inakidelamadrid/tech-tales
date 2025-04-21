0c605bdd23eced4ac6a60ae24256ab61078320bb
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-04-20 18:40:51 -0600

    Add new packages required for markdown processing

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md']
+	// remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+	// rehypePlugins: [rehypeSlug]
+};
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
