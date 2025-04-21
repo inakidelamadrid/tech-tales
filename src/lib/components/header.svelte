1027936bdd7444017b3c86d6d295f713e554712e
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-04-20 18:51:23 -0600

    header links should not be clickable if current route matches href

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,11 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const navLinks = [
+		{ href: '/', label: 'Home' },
+		{ href: '/blog', label: 'Blog' }
+	];
+</script>
+
 <ul class="inline-flex w-full items-center gap-8 pl-4">
-	<!-- <li>
-        <a class="text-non-photo-blue font-mono" href="/blog">Portfolio</a>
-    </li> -->
-	<li>
-		<a href="/blog" class="text-non-photo-blue font-mono">Blog</a>
-	</li>
-	<!-- <li>
-        <a class="text-non-photo-blue font-mono" href="/blog">About</a>
-    </li> -->
+	{#each navLinks as link}
+		<li>
+			{#if page.url.pathname !== link.href}
+				<a href={link.href} class="text-non-photo-blue font-mono">{link.label}</a>
+			{:else}
+				<span class="text-non-photo-blue font-mono">{link.label}</span>
+			{/if}
+		</li>
+	{/each}
 </ul>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import BlogCard from '$lib/components/blog-card.svelte';
+	import Header from '$lib/components/header.svelte';
 	export let data;
 </script>
 
 <section class="mb-16">
+	<Header />
 	<ul class="flex flex-col items-center">
 		{#each data.posts as post}
 			<BlogCard {post} />
