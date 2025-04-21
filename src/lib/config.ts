2b33465f181bf763afddd593c51b1253516496fa
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-04-20 18:29:05 -0600

    add utils and config files

---
+++ src/lib/config.ts
@@ -0,0 +1,4 @@
+import { dev } from '$app/environment';
+export const title = "Your Website's Title";
+export const description = 'A description of your website.';
+export const url = dev ? 'http://localhost:5173' : 'https://yourproductionwebsite.com';
