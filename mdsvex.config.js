069c4e44311c48a7c68620a7825d51a0a05da6b3
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2025-05-24 19:05:23 -0600

    configure shiki to highlight code

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,22 @@
-import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import { defineMDSveXConfig as defineConfig, escapeSvelte } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
+import { createHighlighter } from 'shiki';
 
 // const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
 
 const config = defineConfig({
 	extensions: ['.md', '.svx'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript', 'html']
+			});
+			await highlighter.loadLanguage('javascript', 'typescript', 'html');
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+			return `{@html \`${html}\` }`;
+		}
+	},
 	// remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
 	rehypePlugins: [rehypeSlug],
 	layout: {
